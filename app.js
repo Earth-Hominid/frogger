ed884c0407c4cabc7d857f41730f1c717f86bd39
C <earthhominid@gmail.com>
2021-04-20 11:00:59 -0300

    finalized logic

--- app.js
+++ app.js
@@ -1,81 +1,82 @@
 document.addEventListener('DOMContentLoaded', () => {
-const squares = document.querySelectorAll('.grid div')
-const timeLeft = document.querySelector('#time-left')
-const result = document.querySelector('#result')
-const startBtn = document.querySelector('#button')
-const carsLeft = document.querySelector('.cars-left')
-const carsRight = document.querySelector('.cars-right')
-const logsLeft = document.querySelector('.log-left')
-const logsRight = document.querySelector('.log-right')
-const width = 9
-let currentIndex = 76
-let currentTime = 20
-let timerID
+  
+  const squares = document.querySelectorAll('.grid div')
+  const timeLeft = document.querySelector('#time-left')
+  const result = document.querySelector('#result')
+  const startBtn = document.querySelector('#button')
+  const carsLeft = document.querySelectorAll('.cars-left')
+  const carsRight = document.querySelectorAll('.cars-right')
+  const logsLeft = document.querySelectorAll('.log-left')
+  const logsRight = document.querySelectorAll('.log-right')
+  const width = 9
+  let currentIndex = 76
+  let currentTime = 20
+  let timerID
 
-//render frog on starting block
-squares[currentIndex].classList.add('frog')
-
-//write a function that will move the Frog
-function moveFrog(e) {
-  squares[currentIndex].classList.remove('frog')
-  switch(e.keyCode) {
-    case 37:
-      if(currentIndex % width !== 0) currentIndex -=1
-      break
-    case 38:
-      if(currentIndex - width >= 0) currentIndex -=width
-      break
-    case 39:
-      if(currentIndex % width < width -1) currentIndex +=1
-      break
-    case 40:
-      if(currentIndex + width < width * width) currentIndex +=width
-      break
-  }
+  //render frog on starting block
   squares[currentIndex].classList.add('frog')
-  lose()
-  win()
-}
 
-// move cars
-function autoMoveCars() {
-  carsLeft.forEach(carLeft => moveCarLeft(carLeft))
-  carsRight.forEach(carRight => moveCarRight(carRight))
-}
+  //write a function that will move the Frog
+  function moveFrog(e) {
+    squares[currentIndex].classList.remove('frog')
+    switch(e.keyCode) {
+      case 37:
+        if(currentIndex % width !== 0) currentIndex -=1
+        break
+      case 38:
+        if(currentIndex - width >= 0) currentIndex -=width
+        break
+      case 39:
+        if(currentIndex % width < width -1) currentIndex +=1
+        break
+      case 40:
+        if(currentIndex + width < width * width) currentIndex +=width
+        break
+    }
+    squares[currentIndex].classList.add('frog')
+    lose()
+    win()
+  }
+
+  // move cars
+  function autoMoveCars() {
+    carsLeft.forEach(carLeft => moveCarLeft(carLeft))
+    carsRight.forEach(carRight => moveCarRight(carRight))
+  }
 
 // move the car left on a time loop
 function moveCarLeft(carLeft) {
   switch(true) {
     case carLeft.classList.contains('c1'):
-      carLeft.classList.remove('c1')
-        carLeft.classList.add('c2')
-      break
-      case carLeft.classList.contains('c2'):
-        carLeft.classList.remove('c2')
-          carLeft.classList.add('c3')
-      break
-      case carLeft.classList.contains('c3'):
-        carLeft.classList.remove('c3')
-          carLeft.classList.add('c1')
-      break
+    carLeft.classList.remove('c1')
+    carLeft.classList.add('c2')
+    break
+    case carLeft.classList.contains('c2'):
+    carLeft.classList.remove('c2')
+    carLeft.classList.add('c3')
+    break
+    case carLeft.classList.contains('c3'):
+    carLeft.classList.remove('c3')
+    carLeft.classList.add('c1')
+    break
+    }    
   }
-}
 
-// move the car right on a time loop
-function moveCarRight(carRight) {
+  // move the car right on a time loop
+  function moveCarRight(carRight) {
   switch(true) {
     case carRight.classList.contains('c1'):
-         carRight.classList.remove('c1')
-         carRight.classList.add('c3')
-      break
-      case carRight.classList.contains('c2'):
-           carRight.classList.remove('c2')
-           carRight.classList.add('c1')
-      break
-      case carRight.classList.contains('c3'):
-           carRight.classList.remove('c3')
-           carRight.classList.add('c2')
-      break
+    carRight.classList.remove('c1')
+    carRight.classList.add('c3')
+    break
+    case carRight.classList.contains('c2'):
+    carRight.classList.remove('c2')
+    carRight.classList.add('c1')
+    break
+    case carRight.classList.contains('c3'):
+    carRight.classList.remove('c3')
+    carRight.classList.add('c2')
+    break
   }
 }
 
@@ -178,5 +179,25 @@ function moveWithLogRight() {
   }
 }
 
+//all functions that move pieces.
+function movePieces() {
+  currentTime--
+  timeLeft.textContent = currentTime
+  autoMoveCars()
+  autoMoveLogs()
+  moveWithLogleft()
+  moveWithLogRight()
+  lose()
+}
+
+// to start and pause the game
+startBtn.addEventListener('click', () => {
+  if(timerId) {
+    clearInterval(timerId)
+  } else {
+    timerId = setInterval(movePieces, 1000)
+    document.addEventListener('keyup', moveFrog)
+  }
+})
 
 })
