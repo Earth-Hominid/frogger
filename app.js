2ea59900a07f6f40cc0a1367e9aab03381edceaf
C <earthhominid@gmail.com>
2021-04-20 09:12:11 -0300

    cars and logs logic added

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const squares = document.querySelectorAll('.grid div')
 const timeLeft = document.querySelector('#time-left')
 const result = document.querySelector('#result')
 const startBtn = document.querySelector('#button')
+const carsLeft = document.querySelector('.cars-left')
+const carsRight = document.querySelector('.cars-right')
+const logsLeft = document.querySelector('.log-left')
+const logsRight = document.querySelector('.log-right')
 const width = 9
 let currentIndex = 76
 let timerID
@@ -31,4 +35,56 @@ function moveFrog(e) {
   lose()
   win()
 }
+
+// move cars
+function autoMoveCars() {
+  carsLeft.forEach(carLeft => moveCarLeft(carLeft))
+  carsRight.forEach(carRight => moveCarRight(carRight))
+}
+
+// move the car left on a time loop
+function moveCarLeft(carLeft) {
+  switch(true) {
+    case carLeft.classList.contains('c1'):
+      carLeft.classList.remove('c1')
+        carLeft.classList.add('c2')
+      break
+      case carLeft.classList.contains('c2'):
+        carLeft.classList.remove('c2')
+          carLeft.classList.add('c3')
+      break
+      case carLeft.classList.contains('c3'):
+        carLeft.classList.remove('c3')
+          carLeft.classList.add('c1')
+      break
+  }
+}
+
+// move the car right on a time loop
+function moveCarRight(carRight) {
+  switch(true) {
+    case carRight.classList.contains('c1'):
+         carRight.classList.remove('c1')
+         carRight.classList.add('c3')
+      break
+      case carRight.classList.contains('c2'):
+           carRight.classList.remove('c2')
+           carRight.classList.add('c1')
+      break
+      case carRight.classList.contains('c3'):
+           carRight.classList.remove('c3')
+           carRight.classList.add('c2')
+      break
+  }
+}
+
+// move the logs
+function autoMoveLogs() {
+  logsLeft.forEach(logsLeft => moveLogLeft(logLeft))
+  logsLeft.forEach(logLeft => moveLogLeft(logLeft))
+}
+
+
+
+
 })
